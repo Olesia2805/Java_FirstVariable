e5e52792098baaa168e8225e7525ee9ce98fe455
Olesia2805 <vysotina2002@gmail.com>
2023-09-14 23:26:31 +0200

    First Variable

---
+++ FirstVariable/src/Variable.java
@@ -0,0 +1,11 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		String message = "First Variable"; //assign values ​​to the variable
+		System.out.println(message); //display the first value on screen
+		message = "Change Variable"; //change the value of the variable
+		System.out.println(message); //display the change value on screen
+	}
+
+}
